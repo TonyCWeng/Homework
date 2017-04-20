1beeaf54e4a42f71fc9a462210986abbcc349a53
Tony Weng <tcweng91@gmail.com>
2017-04-20 08:46:38 -0700

    stuff

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'bcrypt'
 gem 'pg'
 
 group :development do
+  gem 'letter_opener'
   gem 'better_errors'
   gem 'binding_of_caller'
   gem 'pry-rails'
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,11 @@
 class UserMailer < ApplicationMailer
+  default from: "everybody@appacademy.io"
+
+  def welcome_email(user)
+    @user = user
+    @url = 'http://example.com/session/new'
+    mail(to: @user.username, subject: 'Welcome to 99 Cats!')
+  end
 end
+
+#returns mail delivery object
